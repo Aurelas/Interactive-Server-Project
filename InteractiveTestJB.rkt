53b079aeb0ae179eee62bc0a3679b011f2336c79
Aurelas <Mikethemantrobus@gmail.com>
2016-04-25 17:06:12 -0400

    Update InteractiveTestJB.rkt

--- InteractiveTestJB.rkt
+++ InteractiveTestJB.rkt
@@ -15,7 +15,6 @@
   (loop 1 x lst)
   )
 
-;Creates a simple image which shows the top goal scorer, and his goal total
 (define (goal-leader)
   (overlay/offset
   (overlay/offset (text "Goals" 20 "gold") 0 28
@@ -57,26 +56,74 @@
                  (text (string-append "T: " (number->string (cadddr x))) 14 "gold")))  
   )
 
-;;Work in progress, will give rank of stats
 
-;(define (get-rank-goals player-id)
-;  (define (helper player-id i rank)
-;    (if (eqv? (get-player player-id) '()) 
-;        rank
-;        (if (null? (get-goals (get-player player-id)))
-;            rank
-;            (if (eqv? (get-player i) '())
-;                rank
-;                (if (null? (get-goals (get-player i)))
-;                    rank
-;                    (if (> (get-goals (get-player player-id)) (get-goals (get-player i)))
-;                        (helper player-id (+ i 1) (- rank 1))
-;                        (helper player-id (+ i 1) rank)))))))
-;  (helper player-id 1 12))
 
-;(get-rank-goals 1)
+
+; Takes a player-id (a number) and gets the goals from that player and compares it to other players
+; ultimately giving a ranking of the player against other players.
+
+(define (get-rank-goals player-id)
+  (define (helper player-id i rank)
+    (if (> i (length aP))
+        (+ rank 1)
+    (if (null? (get-player player-id))
+        rank
+        (if (null? (get-goals (get-player player-id)))
+            rank
+            (if (null? (get-player i))
+                (helper player-id (+ i 1) (length aP))
+                (if (null? (get-goals (get-player i )))
+                    (helper player-id (+ i 1) (length aP))
+                    (if (>= (string->number (get-goals (get-player player-id)) 10) (string->number (get-goals (get-player i)) 10))
+                        (helper player-id (+ i 1 ) (- rank 1))
+                        (helper player-id (+ i 1) rank))))))))
+ 
+  (helper player-id 1 (length aP)))
+
+; Same as above but with 'assists'
+
+(define (get-rank-assists player-id)
+  (define (helper player-id i rank)
+    (if (> i (length aP))
+        (+ rank 1)
+    (if (null? (get-player player-id))
+        rank
+        (if (null? (get-assists (get-player player-id)))
+            rank
+            (if (null? (get-player i))
+                (helper player-id (+ i 1) (length aP))
+                (if (null? (get-assists (get-player i )))
+                    (helper player-id (+ i 1) (length aP))
+                    (if (>= (string->number (get-assists (get-player player-id)) 10) (string->number (get-assists (get-player i)) 10))
+                        (helper player-id (+ i 1 ) (- rank 1))
+                        (helper player-id (+ i 1) rank))))))))
+ 
+  (helper player-id 1 (length aP)))
+
+; Same as above but with 'points'
+
+(define (get-rank-points player-id)
+  (define (helper player-id i rank)
+    (if (> i (length aP))
+        (+ rank 1)
+    (if (null? (get-player player-id))
+        rank
+        (if (null? (get-points (get-player player-id)))
+            rank
+            (if (null? (get-player i))
+                (helper player-id (+ i 1) (length aP))
+                (if (null? (get-points (get-player i )))
+                    (helper player-id (+ i 1) (length aP))
+                    (if (>= (string->number (get-points (get-player player-id)) 10) (string->number (get-points (get-player i)) 10))
+                        (helper player-id (+ i 1 ) (- rank 1))
+                        (helper player-id (+ i 1) rank))))))))
+ 
+  (helper player-id 1 (length aP)))
+
     
 
+; Uses the location on the y-axis of the mouse to report back a plyer-id.
+; Is mainly used on the 'players' screen where plays appear in a list format.
 
   
 
@@ -110,6 +157,8 @@
                                                       0))))))))))))))
 
 
+;Creates the player-stats screen
+; Takes x which is the y-mouse location of the mouse so that we can call the above function inside
 (define (player-stats x)
 (define y (get-player-id-from-click x))
 (above
@@ -146,7 +195,8 @@
 
 
 
-
+; Multiple checks like these throughout the code, just to make sure that if we grab a player with data that hasn't been implemented yet
+; It wont completely break the code.
 (beside
 (overlay (text (if (eqv? (get-player y) '()) "empty" (if (null? (get-number (get-player y))) "empty" (get-number (get-player y)))) 20 "gold")
 (overlay (rectangle 78 48 "solid" "black")
@@ -179,19 +229,23 @@
 (overlay (rectangle 178 48 "solid" "black")
          (rectangle 180 50 "solid" "gold")))
 
-
-(overlay (rectangle 78 48 "solid" "black")
-         (rectangle 80 50 "solid" "gold"))
-
-(overlay (rectangle 78 48 "solid" "black")
-         (rectangle 80 50 "solid" "gold"))
-
+(overlay (text(number->string(get-rank-goals y)) 20 "gold")
 (overlay (rectangle 78 48 "solid" "black")
          (rectangle 80 50 "solid" "gold")))
+
+(overlay (text(number->string(get-rank-assists y)) 20 "gold")
+(overlay (rectangle 78 48 "solid" "black")
+         (rectangle 80 50 "solid" "gold")))
+
+(overlay (text(number->string(get-rank-points y)) 20 "gold")
+(overlay (rectangle 78 48 "solid" "black")
+         (rectangle 80 50 "solid" "gold"))))
 
 
 (rectangle 500 500 "solid" "black")))
 
+
+;Draws the player list image
 (define player-list-image
 (above
 (beside
@@ -340,6 +394,8 @@
 
 ))
 
+; These functions take a list which in this case is the list of all-games defined previously
+; They will then (given a game) parse the string and rip out specific data to be printed to the screen
 
 (define (grab-date lst)
   (car lst))
@@ -347,6 +403,8 @@
   (car (cdr (cdr lst))))
 (define (grab-opp-score lst)
   (car (cdr (cdr (cdr lst)))))
+
+; Creates the games-image
 
 (define (games-image x)
 (above
@@ -365,20 +423,20 @@
          (rectangle 500 40 "solid" "black"))
 (overlay
 (overlay/offset
-         (text "Home Team: " 20 "gold")
+         (text "HomeTeam " 20 "gold")
          300 0
          (text "Opponent: " 20 "gold"))
 (rectangle 500 40 "solid" "black"))
 
 (overlay
 (beside
-        (text "TeamNameHomePH   " 20 "gold")
+        (text "Dragons   " 20 "gold")
 
 (beside
         (text (grab-dragon-score (get-num-list x all-games)) 20 "gold")
         (text " - " 20 "gold")
         (text (grab-opp-score (get-num-list x all-games)) 20 "gold"))
-        (text "  TeamNameOppPH   " 20 "gold"))
+        (text "   Opponent  " 20 "gold"))
 (rectangle 500 100 "solid" "black"))
 
 (rectangle 500 100 "solid" "black")
@@ -411,11 +469,10 @@
         (overlay (goalie-stats (caddr Goalies))
          (rectangle 244 76 "solid" "black")))))
 (rectangle 500 100 "solid" "black")))
-(define tp3 (trending-top-3))
+
 ;Creates the main image to be drawn on screen, and later saved
 (define dragons-image   ;point leaders
-  
-(above 
+(above
 (overlay/offset
 (overlay/offset
 (overlay (text "Dragons" 42 "gold")
@@ -433,18 +490,7 @@
          (rectangle 125 80 "solid" "black"))
         (overlay (win-leader)
          (rectangle 125 80 "solid" "black"))))
-(overlay
-
-(above
- (text "Trending Players" 16 "gold") 
- (text (get-fullname (get-player (car tp3))) 12 "gold")
- (text (get-fullname (get-player (cadr tp3))) 12 "gold")
- (text (get-fullname (get-player (caddr tp3))) 12 "gold"))
-(rectangle 500 100 "solid" "black")
-)
-   
-)
- )
+(rectangle 500 100 "solid" "black")))
 ;dragons-image
 
 (define draw_img dragons-image)
@@ -481,28 +527,50 @@
          draw_img))
  
  
-;(animate create-scene)
+;Processes all of our mouse clicks depeding on where the mouse is when a button is clicked and if we are on the
+; correct screen to process unique click input.
 
 (define (change w x-mouse y-mouse a-key)
   (if (mouse=? a-key "button-down")
       (if (and (< x-mouse 125) (< y-mouse 20))
-          (set! draw_img dragons-image)
+          (begin (set! draw_img dragons-image) (games-image-flag false))
        (if (and (and (> x-mouse 125) (< x-mouse 250)) (< y-mouse 20))
-           (set! draw_img goalie-image)
+           (begin (set! draw_img goalie-image) (games-image-flag false))
            (if (and (and (> x-mouse 250) (< x-mouse 375)) (< y-mouse 20))
-               (set! draw_img player-list-image)                  
+               (begin (set! draw_img player-list-image) (games-image-flag false))                 
                (if (and (eqv? draw_img player-list-image) (and (> x-mouse 50) (and (< y-mouse 500) (> y-mouse 20))))
-                   (set! draw_img (player-stats y-mouse))
+                   (begin (set! draw_img (player-stats y-mouse)) (games-image-flag false))
                    (if (and (> x-mouse 375) (< y-mouse 20))
-                       (set! draw_img (games-image 1))
+                       (begin (set! draw_img (games-image n)) (games-image-flag true))
                        0)))
            
            ))0)
  )
-(big-bang '(75 . 75) (to-draw create-scene) (on-mouse change))
+
+;These are helper functions and defines needed by the key-change function which is used to
+; parse arrow keys sent by the user.
+
+; Flag will keep track if we are on the correct screen of the program to start accepting arrow key input
+(define (games-image-flag f)
+  (set! flag f))
+;defined as false because the first screen of the program does not accept arrow key input
+(define flag #f)
+; N is our 'game number' , when we start to take arrow key input we will traverse the list of games,
+; n keeps track of what game we are on at a specific time
+(define n 1)
 
 
+;Handles arrow key input if we are on the correct screen 
+(define (key-change w a-key) ; w is worldstate and a-key is the key to prcoess
+  (if (equal? flag true)
+  (cond
+    [(key=? a-key "up")     (begin (if (= n 1)(set! n 1)(set! n (- n 1)))(set! draw_img (games-image n)) (games-image-flag true))]
+    [(key=? a-key "down")   (begin (if (= n (length all-games))(set! n (length all-games))(set! n (+ n 1)))(set! draw_img (games-image n)) (games-image-flag true))]
+    [else w])
+  w))
 
+           
+(big-bang '(75 . 75) (to-draw create-scene) (on-mouse change) (on-key key-change))
 
 ; TODO
 ; Use http://m.jdbjohnbrown.net/ as reference
